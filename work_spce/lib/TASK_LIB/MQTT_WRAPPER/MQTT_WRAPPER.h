14c80fda4c9d8377989869e091b2e259c6a3d32d
DrZin <pasawee.dev@gmail.com>
2019-12-31 16:25:14 +0700

    init mqtt wrapper class

---
+++ work_spce/lib/TASK_LIB/MQTT_WRAPPER/MQTT_WRAPPER.h
@@ -0,0 +1,16 @@
+class MQTT_WRAPPER
+{
+private:
+    /* data */
+public:
+    MQTT_WRAPPER(/* args */);
+    ~MQTT_WRAPPER();
+};
+
+MQTT_WRAPPER::MQTT_WRAPPER(/* args */)
+{
+}
+
+MQTT_WRAPPER::~MQTT_WRAPPER()
+{
+}
