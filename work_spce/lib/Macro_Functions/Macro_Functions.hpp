e9e2880adecddaafa7f65f27aeeefa04743c5f69
DrZin <pasawee.dev@gmail.com>
2019-12-31 16:05:03 +0700

    Delate Unuse File

--- work_spce/lib/Macro_Functions/Macro_Functions.hpp
+++
@@ -1,6 +0,0 @@
-#define delay_Time 200
-#define TaskDelay(Time) (delay(Time / portTICK_PERIOD_MS))
-boolean debugging = true;
-#define setDebugging(status) (debugging = status)
-#define Sprintln(payload) (debugging ? Serial.println(payload) : 0)
-#define Sprint(payload) (debugging ? Serial.print(payload) : 0)
