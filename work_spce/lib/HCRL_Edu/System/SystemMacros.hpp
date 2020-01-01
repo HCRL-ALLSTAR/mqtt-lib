2485e03a2d705bfd352f2fe922084f82a6168a90
DrZin <pasawee.dev@gmail.com>
2020-01-01 07:35:33 +0700

    saved

--- work_spce/lib/HCRL_Edu/System/SystemMacros.hpp
+++ work_spce/lib/HCRL_Edu/System/SystemMacros.hpp
@@ -3,7 +3,6 @@
 
 #define delay_Time 200
 #define TaskDelay(Time) (delay(Time / portTICK_PERIOD_MS))
-
 #define Sprintln(payload) (Serial.println(payload))
 #define Sprint(payload) (Serial.print(payload))
 
