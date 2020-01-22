7dc4525ced5aab339a9b4f984b9ee270ff1f8bfa
DrZin-dev <52484219+DrZIn-dev@users.noreply.github.com>
2020-01-22 18:44:38 +0700

    update rgb example

--- work_spce/example/ledRGBExample.cpp
+++ work_spce/example/ledRGBExample.cpp
@@ -1,22 +1,32 @@
 #include <Arduino.h>
-#include "HCRL_Edu.h"
-
-HCRL_Edu hcrl;
+#include "HCRL_EDU.h"
+millisDelay ledRand;
+HCRL_EDU hcrl;
 
 void setup()
 {
-    Serial.begin(Defalult_Baud_Rate);
-    hcrl.LedRGB.begin();
+    Serial.begin(115200);
 
-    hcrl.LedRGB.setBrightness(10);
+    hcrl.RGB_LED.begin();
+    hcrl.RGB_LED.setBrightness(50);
+
+    hcrl.RGB_STRIP.begin();
+    hcrl.RGB_STRIP.setBrightness(255);
 }
 
 void loop()
 {
-    for (int i = 0; i < 3; i++)
+    if (ledRand.justFinished())
     {
-        hcrl.LedRGB.setPixelsColor(i, random(255), random(255), random(255));
+        for (int i = 0; i < RGB_LED_PIXELS; i++)
+        {
+            hcrl.RGB_LED.setPixelsColor(i, random(255), random(255), random(255));
+        }
+        for (int i = 0; i < RGB_STRIP_PIXELS; i++)
+        {
+            hcrl.RGB_STRIP.setPixelsColor(i, random(255), random(255), random(255));
+        }
+        ledRand.repeat();
     }
-    randomSeed(millis());
-    hcrl.update(); //Update Data
+    hcrl.update();
 }
